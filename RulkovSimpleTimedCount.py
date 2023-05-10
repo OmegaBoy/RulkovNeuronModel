247fb4c1d4ccbf03c7a1d67f02a81608fbdd552f
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2023-05-10 16:09:36 -0300

    Incremented time

--- RulkovSimpleTimedCount.py
+++ RulkovSimpleTimedCount.py
@@ -7,11 +7,11 @@ from Rulkov import Rulkov
 # %% RULKOV
 rulkov = Rulkov()
 # %% Initial Simulation
-rulkov.RulkovSimple(alpha=4.8, beta=0.001, sigma=0.001, x0=-2, y0=-2.9, N=100)
+rulkov.RulkovSimple(alpha=4.8, beta=0.001, sigma=0.001, x0=-2, y0=-2.9, N=10000)
 # %% PLOTTING
 plotting = Plotting()
 noiseDevPar = (rulkov.noiseDev, 0, 1, "noiseDev", rulkov.ChangeParameterCountSpikes)
 tresholdPar = (rulkov.threshold, 0, 3, "threshold", rulkov.ChangeParameterCountSpikes)
 pars = (noiseDevPar, tresholdPar)
-scale=1
+scale=100
 plotting.SliderPlot(datas=[rulkov.SpikePlots()[0], rulkov.CountSpikes(noiseDev=0, threshold=0.5)], step=rulkov.N/scale, zoom=0.8, together=True, extraSliders=pars)
